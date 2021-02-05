a78b2ebcadf7e830e10dcfbdc4e8f7639cff2415
Taylor Herndon <TaylorBryanHerndon@gmail.com>
2021-02-05 08:25:43 -0700

    Add project files.

---
+++ ConvertValidate.vb
@@ -0,0 +1,57 @@
+﻿Option Strict On
+Option Explicit On
+
+'Taylor Herndon
+'RCET0265
+'Spring 2021
+'Convert and Validate
+'
+
+Module ConvertValidate
+
+    Sub Main()
+
+        Dim MyString As String = ""
+        Dim MyInteger As Int32 = 0
+
+        'Tell the user to give us a string
+        Console.WriteLine("Hello!")
+        Console.WriteLine("Please give me a string of numbers to convert into real numbers!")
+        MyString = Console.ReadLine()
+
+        'Call funciton and write out the result and our variable
+        Console.WriteLine(ConvertValidate.ConvertStringToInteger(MyString, MyInteger))
+        Console.WriteLine(MyInteger)
+
+
+        Console.ReadKey()
+
+    End Sub
+
+    Function ConvertStringToInteger(ByVal ConvertThisString As String, ByRef ToThisInteger As Int32) As String
+
+        Try
+
+            'Try to convert the string to a integer
+            ToThisInteger = Convert.ToInt32(ConvertThisString)
+            Return "[Success]"
+
+        Catch ex As Exception
+
+            If ConvertThisString = "" Then
+
+                'If string has nothing in it then return empty
+                Return "[Error]: Empty"
+
+            Else
+
+                'If a different error occoured it is most likely that the user did not input only numbers
+                Return "[Error]: String does not contain only numbers."
+
+            End If
+
+        End Try
+
+    End Function
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Convert and Validate")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Convert and Validate")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b4fd42b4-eede-4769-97e8-5cd637e57d6a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
